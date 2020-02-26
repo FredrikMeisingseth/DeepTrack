35b23a5131f17b0023cacfceff2f4c0e085d18d2
Adrian Lundell <36153706+AdrianLundell@users.noreply.github.com>
2020-02-26 10:00:26 +0100

    Slight fix

--- DeepTrack 1.0/benchmark.py
+++ DeepTrack 1.0/benchmark.py
@@ -1,7 +1,7 @@
 def calculate_variance(coordinate_batch):
     #input: coordinate_batch: (n,m,2)-list for n samples with m number of particles
     #
-    for i in range 
+    for i in range:
     for t in range(1,coordinate_batch.shape[0]-1):
         result = result + (coordinate_batch[t,,i+1]-coordinate_batch[t-1])(coordinate_batch[i]-coordinate_batch[i-1])
 	return result/coordinate_batch.len()
@@ -14,7 +14,7 @@ def track_particle_method(batch):
     #Implementation of other tracking methods
 
 
-def benchmark_SNR(network, batch_size):
+def benchmark_SNR(network, batch):
     from time import time
     import imageGeneration
 
