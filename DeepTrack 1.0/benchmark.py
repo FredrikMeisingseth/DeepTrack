54db036524c920ca55d32e278041087b6f2bffb5
dtonderski <dtonderski@gmail.com>
2020-04-17 18:43:20 +0200

    Added distance from upper left method

--- DeepTrack 1.0/benchmark.py
+++ DeepTrack 1.0/benchmark.py
@@ -1,4 +1,4 @@
-def operating_characteristics(labels, predictions):
+def get_operating_characteristics(labels, predictions):
     """
     Method that returns the operating characteristics of a prediction.
     Input:
@@ -27,3 +27,20 @@ def operating_characteristics(labels, predictions):
     FN = sum(label_first_feature * (1 - prediction_first_feature))
 
     return P, N, TP, TN, FP, FN
+
+
+def distance_from_upper_left_corner_ROC(operating_characteristics, FPR_weight = 1.0):
+    """
+    Method that calculates the distance from the upper left corner of the ROC space for a given TPR and FPR
+    Inputs:
+        TPR - True Positive Rate
+        FPR - False Positive Rate
+        FPR_weight - instead of just FPR, FPR*FPR_weight is used in the calculation. This is usually a value between 0
+                     and 1. A small value means that changes in FPR affect the distance less.
+    """
+    P, N, TP, TN, FP, FN = operating_characteristics
+
+    TPR = TP/P
+    FPR = FP/N
+
+    return ((1-TPR)**2 + (FPR*FPR_weight)**2) ** (1/2)
