6316875bb93c4a988d4e215ded92fde54364f5df
dtonderski <dtonderski@gmail.com>
2020-04-17 20:15:44 +0200

    Created scanning box operating characteristics method

--- DeepTrack 1.0/benchmark.py
+++ DeepTrack 1.0/benchmark.py
@@ -29,7 +29,83 @@ def get_operating_characteristics(labels, predictions):
     return P, N, TP, TN, FP, FN
 
 
-def distance_from_upper_left_corner_ROC(operating_characteristics, FPR_weight = 1.0):
+def get_operating_characteristics_scanning_box(predicted_particle_positions_x, predicted_particle_positions_y,
+                                               particle_positions_x, particle_positions_y,
+                                               image_size_x, image_size_y,
+                                               scanning_box_size_x, scanning_box_size_y):
+    """ Method that returns the operating characteristics using a scanning box approach.
+    Inputs:
+    predicted_particle_positions_x: list of predicted x-coordinates of the particles
+    predicted_particle_positions_y: list of predicted y-coordinates of the particles
+    particle_positions_x: list of x-coordinates of the particles
+    particle_positions_y: list of y-coordinates of the particles
+    image_size_x: x-size of the image. We represent images as matrices, so this is usually the vertical side-length.
+    image_size_y: y-size of the image. This is usually the horizontal side length.
+    scanning_box_size_x: x-size of the scanning box. If image_size_x isn't divisible by scanning_box_size_x, the image
+        is extended to make it fit the extra part of the scanning box.
+    scanning_box_size_y: y-size of the scanning box. If image_size_y isn't divisible by scanning_box_size_y, the image
+        is extended to make it fit the extra part of the scanning box.
+
+    Notes:
+        1) If a particle is exactly on the edge of the last scanning box, it will not be counted
+
+    Outputs:
+        operating_characteristics
+    """
+    from numpy import meshgrid, arange, ndarray
+    scanning_box_coordinates_x, scanning_box_coordinates_y = meshgrid(arange(0, image_size_x, scanning_box_size_x),
+                                                                      arange(0, image_size_y, scanning_box_size_y),
+                                                                      sparse=False,
+                                                                      indexing='ij')
+
+    operating_characteristics = [0, 0, 0, 0, 0, 0]
+    for scanning_box_start_x, scanning_box_start_y in zip(ndarray.flatten(scanning_box_coordinates_x),
+                                                                                ndarray.flatten(scanning_box_coordinates_y)):
+        scanning_box_stop_x = scanning_box_start_x + scanning_box_size_x
+        scanning_box_stop__y = scanning_box_start_y + scanning_box_size_y
+        particles_in_box = 0
+        predicted_particles_in_box = 0
+
+        for particle_position_x, particle_position_y in zip(particle_positions_x, particle_positions_y):
+            if scanning_box_start_x <= particle_position_x < scanning_box_stop_x:
+                if scanning_box_start_y <= particle_position_y < scanning_box_stop__y:
+                    particles_in_box += 1
+
+        for predicted_particle_position_x, predicted_particle_position_y in zip(predicted_particle_positions_x,
+                                                                                predicted_particle_positions_y):
+            if scanning_box_start_x <= predicted_particle_position_x < scanning_box_stop_x:
+                if scanning_box_start_y <= predicted_particle_position_y < scanning_box_stop__y:
+                    predicted_particles_in_box += 1
+
+        P_box = particles_in_box
+        if particles_in_box == 0:
+            N_box = 1
+            TP_box = 0
+            FN_box = 0
+            if predicted_particles_in_box == 0:
+                FP_box = 0
+                TN_box = 1
+            else:
+                FP_box = predicted_particles_in_box
+                TN_box = 0
+        else:
+            N_box = 0
+            TN_box = 0
+            if predicted_particles_in_box >= particles_in_box:
+                TP_box = particles_in_box
+                FP_box = predicted_particles_in_box - particles_in_box
+                FN_box = 0
+            else:
+                TP_box = predicted_particles_in_box
+                FP_box = 0
+                FN_box = particles_in_box - predicted_particles_in_box
+
+        operating_characteristics_box = [P_box, N_box, TP_box, FP_box, TN_box, FN_box]
+        operating_characteristics = [sum(x) for x in zip(operating_characteristics, operating_characteristics_box)]
+
+    return operating_characteristics
+
+def distance_from_upper_left_corner_ROC(operating_characteristics, FPR_weight=1.0):
     """
     Method that calculates the distance from the upper left corner of the ROC space for a given TPR and FPR
     Inputs:
@@ -40,25 +116,24 @@ def distance_from_upper_left_corner_ROC(operating_characteristics, FPR_weight = 
     """
     P, N, TP, TN, FP, FN = operating_characteristics
 
-    TPR = TP/P
-    FPR = FP/N
+    TPR = TP / P
+    FPR = FP / N
 
-    return ((1-TPR)**2 + (FPR*FPR_weight)**2) ** (1/2)
+    return ((1 - TPR) ** 2 + (FPR * FPR_weight) ** 2) ** (1 / 2)
 
 
 def centroids_DT(
-    particle_positions_x,
-    particle_positions_y,
-    particle_radial_distance,
-    particle_interdistance,
-    ):
-
+        particle_positions_x,
+        particle_positions_y,
+        particle_radial_distance,
+        particle_interdistance,
+):
     import numpy as np
 
     particle_number = 0
     particle_index = 0
     particle_numbers = -np.ones(len(particle_positions_x))
-    
+
     # Sort all predicted points to correct particle 
     while particle_numbers[np.argmin(particle_numbers)] == -1:
         particle_index = np.argmin(particle_numbers)
@@ -68,8 +143,8 @@ def centroids_DT(
         for j in range(len(particle_positions_x)):
 
             if (particle_positions_x[j] - particle_positions_x[particle_index]) ** 2 \
-                + (particle_positions_y[j] - particle_positions_y[particle_index]) ** 2 \
-                < particle_interdistance ** 2:
+                    + (particle_positions_y[j] - particle_positions_y[particle_index]) ** 2 \
+                    < particle_interdistance ** 2:
                 particle_numbers[j] = particle_numbers[particle_index]
 
     centroid_x = np.zeros(int(np.amax(particle_numbers)) + 1)
@@ -92,10 +167,9 @@ def centroids_DT(
 
 
 def get_predicted_positions_DT(particle_radial_distance_threshold,
-                                            particle_maximum_interdistance,
-                                            number_frames_to_be_tracked,
-                                            predicted_positions_wrt_frame):
-
+                               particle_maximum_interdistance,
+                               number_frames_to_be_tracked,
+                               predicted_positions_wrt_frame):
     import numpy as np
     import matplotlib.pyplot as plt
 
@@ -103,7 +177,7 @@ def get_predicted_positions_DT(particle_radial_distance_threshold,
     particle_centroids = []
     particle_radial_distance = []
     predicted_positions = []
-    
+
     for i in range(number_frames_to_be_tracked):
 
         particle_positions_x = []
@@ -123,7 +197,7 @@ def get_predicted_positions_DT(particle_radial_distance_threshold,
                         np.append(particle_radial_distance,
                                   predicted_positions_wrt_frame[i, j, k, 2])
 
-        if(len(particle_positions_x) == 0):
+        if (len(particle_positions_x) == 0):
             raise KeyError('particle_radial_distance_threshold too small, no predictions passed')
 
         particle_positions.append([])
@@ -132,20 +206,21 @@ def get_predicted_positions_DT(particle_radial_distance_threshold,
 
         # Calculate the centroid positions
         (centroids_x, centroids_y) = centroids_DT(particle_positions_x,
-                                               particle_positions_y, 
-                                               particle_radial_distance,
-                                               particle_maximum_interdistance)
-        
-        for k in range(len(centroids_x)):
-            predicted_positions.append((i,centroids_x[k],centroids_y[k]))
-        
-    return predicted_positions
-        
+                                                  particle_positions_y,
+                                                  particle_radial_distance,
+                                                  particle_maximum_interdistance)
 
-def get_predicted_positions_unet(number_frames_to_be_tracked, batch_predictions,video_width, video_height, cutoff_value = 0.9):
+        for k in range(len(centroids_x)):
+            predicted_positions.append((i, centroids_x[k], centroids_y[k]))
+
+    return predicted_positions
+
+
+def get_predicted_positions_unet(number_frames_to_be_tracked, batch_predictions, video_width, video_height,
+                                 cutoff_value=0.9):
     import imageGeneration as IG
     predicted_positions = []
-    predictions = IG.cutoff(batch_predictions,cutoff_value, apply_sigmoid=True)
+    predictions = IG.cutoff(batch_predictions, cutoff_value, apply_sigmoid=True)
 
     for i in range(number_frames_to_be_tracked):
 
@@ -155,13 +230,14 @@ def get_predicted_positions_unet(number_frames_to_be_tracked, batch_predictions,
             x_position = x_mean_list[j]
             y_position = y_mean_list[j]
             frame_index = i
-            if(x_position <= video_width and y_position <= video_height):
-                predicted_positions.append((frame_index,x_position,y_position))
-    
+            if (x_position <= video_width and y_position <= video_height):
+                predicted_positions.append((frame_index, x_position, y_position))
+
     return predicted_positions
 
 
-def hits_and_misses(number_frames_to_be_tracked, predicted_positions, particle_positions_and_radiuses, return_misses = False):
+def hits_and_misses(number_frames_to_be_tracked, predicted_positions, particle_positions_and_radiuses,
+                    return_misses=False):
     from numpy import sqrt, zeros
 
     nr_predictions = zeros(number_frames_to_be_tracked)
@@ -170,44 +246,45 @@ def hits_and_misses(number_frames_to_be_tracked, predicted_positions, particle_p
     nr_misses = zeros(number_frames_to_be_tracked)
     misses = []
 
-
     for i in range(int(number_frames_to_be_tracked)):
         predictions_for_frame_i = []
         for j in range(len(predicted_positions)):
-            if(predicted_positions[j][0] == i):
+            if (predicted_positions[j][0] == i):
                 predictions_for_frame_i.append(predicted_positions[j])
-        
+
         nr_predictions[i] = len(predictions_for_frame_i)
         nr_real_particles[i] = len(particle_positions_and_radiuses[i][0])
-        
+
         for j in range(int(nr_predictions[i])):
             hasHit = False
             for part in range(int(nr_real_particles[i])):
-                distance_x = (predictions_for_frame_i[j][1] - particle_positions_and_radiuses[i][0][part])**2
-                distance_y = (predictions_for_frame_i[j][2] - particle_positions_and_radiuses[i][1][part])**2
+                distance_x = (predictions_for_frame_i[j][1] - particle_positions_and_radiuses[i][0][part]) ** 2
+                distance_y = (predictions_for_frame_i[j][2] - particle_positions_and_radiuses[i][1][part]) ** 2
                 distance = sqrt(distance_x + distance_y)
 
-                if(distance < particle_positions_and_radiuses[i][2][part]):
-                    hasHit = True  
+                if (distance < particle_positions_and_radiuses[i][2][part]):
+                    hasHit = True
 
-            if(hasHit):nr_hits[i] += 1 
-            else: misses.append(predictions_for_frame_i[j])
-                        
-        nr_misses[i] = nr_predictions[i] - nr_hits[i]          
-    
+            if (hasHit):
+                nr_hits[i] += 1
+            else:
+                misses.append(predictions_for_frame_i[j])
 
-    if(return_misses):
-        return nr_real_particles,nr_predictions,nr_hits,nr_misses,misses
+        nr_misses[i] = nr_predictions[i] - nr_hits[i]
+
+    if (return_misses):
+        return nr_real_particles, nr_predictions, nr_hits, nr_misses, misses
     else:
-        return nr_real_particles,nr_predictions,nr_hits,nr_misses
+        return nr_real_particles, nr_predictions, nr_hits, nr_misses
 
 
-def visualize_hits_and_misses(number_frames_to_be_tracked, frames,  particle_positions_and_radiuses, predicted_positions, misses):
+def visualize_hits_and_misses(number_frames_to_be_tracked, frames, particle_positions_and_radiuses, predicted_positions,
+                              misses):
     import matplotlib.pyplot as plt
-    
+
     ### Visualize tracked frames
     for i in range(number_frames_to_be_tracked):
-        
+
         # Show frame
         fig = plt.figure(figsize=(10, 10))
         plt.imshow(frames[i], cmap='gray', vmin=0, vmax=1)
@@ -215,18 +292,20 @@ def visualize_hits_and_misses(number_frames_to_be_tracked, frames,  particle_pos
 
         # Threshold the radial distance of the predicted points
         for j in range(int(len(predicted_positions))):
-            if(predicted_positions[j][0] == i):
-                if(predicted_positions[j] in misses):
+            if (predicted_positions[j][0] == i):
+                if (predicted_positions[j] in misses):
                     # Plot the predicted points
                     plt.plot(predicted_positions[j][2],
-                            predicted_positions[j][1], '.r')
+                             predicted_positions[j][1], '.r')
                 else:
                     # Plot the predicted points
                     plt.plot(predicted_positions[j][2],
-                            predicted_positions[j][1], '.b')
-        
+                             predicted_positions[j][1], '.b')
+
         for part in range(nr_real_particles):
             plt.plot(particle_positions_and_radiuses[i][1][part],
-                                particle_positions_and_radiuses[i][0][part], '^m')
-            circle = plt.Circle((particle_positions_and_radiuses[i][1][part], particle_positions_and_radiuses[i][0][part]), particle_positions_and_radiuses[i][2][part], color='m', fill=False)
+                     particle_positions_and_radiuses[i][0][part], '^m')
+            circle = plt.Circle(
+                (particle_positions_and_radiuses[i][1][part], particle_positions_and_radiuses[i][0][part]),
+                particle_positions_and_radiuses[i][2][part], color='m', fill=False)
             plt.gcf().gca().add_artist(circle)
